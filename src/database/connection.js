01adf18a566f584836ab9d38ddfc7907053007c3
vargasjda <juandajda.jda@gmail.com>
2024-11-05 18:48:22 -0500

    Descripción de los cambios realizados

--- src/database/connection.js
+++ src/database/connection.js
@@ -4,21 +4,15 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: 'mysql',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'mysql',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false 
+    }
   }
-);
+});
 
 module.exports = sequelize;

1659db67ce0966c4a05c139302972a8895486d12
vargasjda <juandajda.jda@gmail.com>
2024-11-05 19:22:11 -0500

    Descripción de los cambios realizados

--- src/database/connection.js
+++ src/database/connection.js
@@ -5,16 +5,14 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 
-const sequelize  = new Sequelize(
-  process.env.DB_NAME,        // Nombre de la base de datos
-  process.env.DB_USER,        // Usuario de la base de datos
-  process.env.DB_PASSWORD,    // Contraseña de la base de datos
-  {
-    host: process.env.DB_HOST,     // Host de la base de datos
-    port: process.env.DB_PORT,     // Puerto de la base de datos
-    dialect: 'mysql',              // El tipo de base de datos que estamos usando
-    logging: false,                // Desactivar logs, si es necesario
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'mysql',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false 
+    }
   }
-);
+});
 
 module.exports = sequelize;
